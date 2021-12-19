6afc4da4bd59726a2e51cc8d3a9f152ba869f776
Christoph Willing <chris.willing@linux.com>
2021-12-19 11:44:33 +1000

    Add events.py demo
    Signed-off-by: Christoph Willing <chris.willing@linux.com>

---
+++ api/events.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+Copyright (c) 2021  Christoph Willing, Brisbane Australia
+SPDX-License-Identifier: MIT
+"""
+
+import sys, os
+import paho.mqtt.client as mqtt
+import json
+
+
+def main():
+
+	def on_connect(client, userdata, flags, rc):
+		print("Connected with result code "+str(rc))
+		client.subscribe("/xkeys/server/#")
+
+	def on_subscribe(client, userdata, mid, granted_qos):
+		print("Subscribed OK");
+
+	def on_message(client, userdata, msg):
+		print(msg.topic)
+		print(json.dumps(json.loads(msg.payload), indent=2))
+		print()
+
+	client = mqtt.Client()
+	client.on_connect = on_connect
+	client.on_message = on_message
+	client.on_subscribe = on_subscribe
+
+	client.connect("localhost", 1883, 60)
+
+	client.loop_forever()
+
+
+if __name__ == '__main__':
+    main()
+
