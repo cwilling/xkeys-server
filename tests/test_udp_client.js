0d2a724bc0280ccf9459fce9fc0b338406016421
Christoph Willing <chris.willing@linux.com>
2022-02-03 20:58:00 +1000

    Send requests as per api
    Signed-off-by: Christoph Willing <chris.willing@linux.com>

--- tests/test_udp_client.js
+++ tests/test_udp_client.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 
 
+/*	test_udp_client.js
+*
+*	SPDX-License-Identifier: MIT OR LGPL-2.0-or-later
+*	SPDX-FileCopyrightText: 2022 Christoph Willing <chris.willing@linux.com>
+* 
+*	A client for testing UDP communication with an xkeys-server using the
+*	API described at https://gitlab.com/chris.willing/xkeys-server/-/tree/main/api
+*/
+
+
 const dgram = require('dgram');
 const client = dgram.createSocket('udp4');
 
@@ -18,19 +28,73 @@ if (myArgs.length > 1) {
 }
 console.log(`Connecting to ${host}:${port}`);
 
-const message = new Buffer.from('Hello Server', "UTF-8");
 
 client.on('message', (message, remote) => {
-	console.log('Msg Rcvd: ' + JSON.stringify(JSON.parse(message), null, 2) + "  from: " + JSON.stringify(remote));
-});
-
-client.send(message, 0, message.length, port, host, (err, bytes) => {
-	if (err) {
-		throw err;
+	//console.log('Msg Rcvd: ' + JSON.stringify(JSON.parse(message), null, 2) + "  from: " + JSON.stringify(remote));
+	//console.log(`Msg Rcvd: ${JSON.stringify(JSON.parse(message))} from: ${JSON.stringify(remote)})`);
+	var msg = "";
+	try{
+		msg = JSON.parse(message);
+		if (msg.request == "result_EOI") {
+			if (msg.data == "OK") {
+				console.log(`EOI was accepted by ${msg.sid}`);
+			} else {
+				console.log(`EOI not accepted, returned: ${JSON.stringify(msg)}`);
+			}
+		} else if (msg.request == "result_deviceList") {
+			var device_keys = Object.keys(msg.data);
+			if (device_keys.length > 0 ){
+				/* Choose a device randomly to display */
+				var choice = Math.floor(Math.random() * device_keys.length);
+				console.log(`Showing number ${choice} of ${device_keys.length} attached devices:`);
+				console.log(JSON.stringify(msg.data[device_keys[choice]], null, 2));
+			} else {
+				console.log(`No devices attached at ${msg.sid}`);
+			}
+		} else if (msg.request == "result_productList") {
+			var product_keys = Object.keys(msg.data);
+			if (product_keys.length > 0 ){
+				/* Choose a device randomly to display */
+				var choice = Math.floor(Math.random() * product_keys.length);
+				console.log(`Showing number ${choice} of ${product_keys.length} products:`);
+				console.log(JSON.stringify(msg.data[product_keys[choice]]));
+			} else {
+			}
+		} else if (msg.request == "device_event") {
+			//console.log(JSON.stringify(JSON.parse(message)));
+			/* FILTER for only "down" events
+			*  - could be "up", "tbar", "jog", "shuttle", ...
+			*
+			*	To see without filtering i.e. see all events
+			*	remove (or comment out) the following 'if' statement
+			*/
+			if (msg.data.type == "down") {
+				//console.log(JSON.stringify(msg, null, 2));
+				console.log(msg);
+			}
+		} else {
+		}
 	}
-	console.log(`EOI sent to ${host}:${port}`);
+	catch (err) {
+	}
 });
 
+send_udp_message = (message) => {
+	var msg = "";
+	try {
+		msg = JSON.parse(message);
+	}
+	catch (err) {
+		console.log(`Not sending invalid message: ${message}`);
+		console.log(err);
+	}
+	client.send(message, 0, message.length, port, host, (err, bytes) => {
+		if (err) {
+			throw err;
+		}
+		console.log(`Sending ${msg.request} request to ${host}:${port}`);
+	});
+}
 
 /* Occasional msg back to server */
 repeat_msg = () => {
@@ -42,4 +106,15 @@ repeat_msg = () => {
 		console.log(`Repeat msg sent to ${host}:${port}`);
 	});
 }
-setInterval(repeat_msg, 9000)
+//setInterval(repeat_msg, 9000);
+
+
+/* This is where we start doing things:
+*	- send EOI
+*	- some time later, request a list of devices attached to the server
+*   - some time later, request a list of known products
+*/
+send_udp_message(new Buffer.from('{"request":"EOI"}', 'UTF-8'));
+setTimeout(send_udp_message, 9000, (new Buffer.from('{"request":"deviceList"}', 'UTF-8')));
+setTimeout(send_udp_message, 18000, (new Buffer.from('{"request":"productList"}', 'UTF-8')));
+
