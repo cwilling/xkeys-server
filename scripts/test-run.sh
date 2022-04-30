59cb0c7fda3252956ea39a93641c297a9d55e148
Christoph Willing <chris.willing@linux.com>
2022-04-30 13:05:41 +1000

    Add test-run.sh for CI/CD
    Signed-off-by: Christoph Willing <chris.willing@linux.com>

---
+++ scripts/test-run.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd $(dirname $0)
+while read -r line; do
+  echo $line;
+  if [ "$line" = "Are we there yet?" ]; then
+    echo "All done"
+	pkill node
+    exit 0
+  fi;
+done < <(./xkeys-server.js )
+
+
