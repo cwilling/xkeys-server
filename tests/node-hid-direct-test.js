e88f9aa5ff69764c56cbafd959e2c19a734ee187
Christoph Willing <chris.willing@linux.com>
2022-07-20 10:34:57 +1000

    More logging
    Signed-off-by: Christoph Willing <chris.willing@linux.com>

--- tests/node-hid-direct-test.js
+++ tests/node-hid-direct-test.js
@@ -7,9 +7,13 @@ process.env.UV_THREADPOOL_SIZE = 24;
 
 const VENDORID = 1523;
 
+var count = 0;
 HID.devices().forEach( (deviceInfo) => {
 	if (deviceInfo.vendorId == VENDORID && deviceInfo.usagePage == 12 && deviceInfo.usage == 1) {
-		console.log(`Found ${JSON.stringify(deviceInfo, null, 2)}`);
+		count += 1;
+		console.log(`Found device entry ${count}`);
+		console.log(`${JSON.stringify(deviceInfo, null, 2)}`);
+		console.log();
 		var device = new HID.HID( deviceInfo.path );
 		device.on("data", function(data) {
 			console.log(`received: ${JSON.stringify(data)}`);
